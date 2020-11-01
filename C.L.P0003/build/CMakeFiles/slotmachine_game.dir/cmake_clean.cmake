7c15cfcb52487393a499cad1f0b627399925da8d
hailiang194 <hailiang194>
2020-11-01 17:47:13 +0700

    Init for C.L.P0003 Project

---
+++ C.L.P0003/build/CMakeFiles/slotmachine_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slotmachine_game.dir/src/main.cpp.o"
+  "slotmachine_game"
+  "slotmachine_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slotmachine_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
